a835927bfee33ad9af1e61b5de7962a2e41e840d
giorgi <giorgiodishvili>
2021-06-25 11:14:11 +0400

    blink

--- app/src/main/java/com/android/quizapp/ui/passwordsecurity/PasswordSecurityFragment.kt
+++ app/src/main/java/com/android/quizapp/ui/passwordsecurity/PasswordSecurityFragment.kt
@@ -1,10 +1,10 @@
 package com.android.quizapp.ui.passwordsecurity
 
-import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.GridLayoutManager
 import com.android.football.base.BaseFragment
@@ -22,34 +22,32 @@ class PasswordSecurityFragment : BaseFragment<PasswordSecurityFragmentBinding>(P
         binding.recyclerView.adapter = SecurityAdapter(object : SecurityItemCallback{
             override fun onClickItem(absoluteAdapterPosition: Int) {
                 inputPass += absoluteAdapterPosition.toString()
-                val length = inputPass.length
-                when(length){
+                when(inputPass.length){
                     1 -> binding.dot1.background = (R.drawable.roundall_green.toDrawable())
                     2 -> binding.dot2.background = (R.drawable.roundall_green.toDrawable())
                     3 -> binding.dot3.background = (R.drawable.roundall_green.toDrawable())
-                    4 -> binding.dot4.background = (R.drawable.roundall_green.toDrawable())
-                }
-
-                if(length == 4){
-                    when {
-                        rememberInput.isEmpty() -> {
-                            binding.enterPassTv.text = "Repeat Passcode"
-                            rememberInput = inputPass
+                    4 -> {
+                        binding.dot4.background = (R.drawable.roundall_green.toDrawable())
+                        when {
+                            rememberInput.isEmpty() -> {
+                                binding.enterPassTv.text = "Repeat Passcode"
+                                rememberInput = inputPass
+                            }
+                            rememberInput == inputPass -> {
+                                binding.enterPassTv.text = "Enter your passcode"
+                                Toast.makeText(requireContext(),"ACCEPTED",Toast.LENGTH_SHORT).show()
+                            }
+                            else -> {
+                                binding.enterPassTv.text = "Enter your passcode"
+                                Toast.makeText(requireContext(),"WRONG",Toast.LENGTH_SHORT).show()
+                            }
                         }
-                        rememberInput == inputPass -> {
-                            binding.enterPassTv.text = "Enter your passcode"
-                            Toast.makeText(requireContext(),"ACCEPTED",Toast.LENGTH_SHORT).show()
-                        }
-                        else -> {
-                            binding.enterPassTv.text = "Enter your passcode"
-                            Toast.makeText(requireContext(),"WRONG",Toast.LENGTH_SHORT).show()
-                        }
+                        inputPass=""
+                        binding.dot1.background = (R.drawable.roundall.toDrawable())
+                        binding.dot2.background = (R.drawable.roundall.toDrawable())
+                        binding.dot3.background = (R.drawable.roundall.toDrawable())
+                        binding.dot4.background = (R.drawable.roundall.toDrawable())
                     }
-                    inputPass=""
-                    binding.dot1.background = (R.drawable.roundall.toDrawable())
-                    binding.dot2.background = (R.drawable.roundall.toDrawable())
-                    binding.dot3.background = (R.drawable.roundall.toDrawable())
-                    binding.dot4.background = (R.drawable.roundall.toDrawable())
                 }
             }
         })
